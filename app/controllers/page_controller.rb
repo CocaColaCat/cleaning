f4f99b8c3934c2281956b89eaec789a152718331
CocaColaCat <jianping.zhou.1986@gmail.com>
2014-04-20 13:59:38 +0800

    added page controller with desired actions to route to required pages.

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,13 @@
+class PageController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Cleaning::Application.routes.draw do
+  get "page/home"
+
+  get "page/about"
+
+  get "page/services"
+
+  get "page/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
