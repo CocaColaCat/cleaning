01ca239606a84d7610e337546f1ee48b8129e80a
CocaColaCat <jianping.zhou.1986@gmail.com>
2014-04-27 13:49:55 +0800

    added bootstrap fonts and js datepicker

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -1,7 +1,13 @@
 class Enquiry < ActiveRecord::Base
   
-  validates :contact_number, :date, :email, :location, :name, :service, presence: true
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :message => 'Please provide a valid email address.'
+  SERVICES = ['Carpet Cleaning', 'Office Cleaning', 'Domestic Cleaning', 'End of Tenacy']
+  validates :contact_number, :date, :location, :name, :service, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :message => 'Please provide a valid email address.', :if => :email_provided?
 
   attr_accessible :contact_number, :date, :details, :email, :location, :name, :service
+
+  def email_provided?
+      self.email != ''    
+  end
+
 end
