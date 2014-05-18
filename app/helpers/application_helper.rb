ca57c826f64e76923fcd612afe2b71c62e91f084
CocaColaCat <jianping.zhou.1986@gmail.com>
2014-05-18 18:08:03 +0800

    highlight current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def nav_link(link_text, link_path, is_top_parent)
+    class_name = current_page?(link_path) ? 'current' : ' '
+    class_name += (is_top_parent ? ' top_parent' : '')
+    
+    link_to link_text, link_path, :class => class_name
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Cleaning::Application.routes.draw do
+  root :to => 'page#home'
   get "enquiry/new"
 
   post "enquiry/create"
-
-  get "page/home"
 
   get "page/about"
 
   get "page/services"
 
   get "page/contact"
-
 end
